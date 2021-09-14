9bd02fc2f844d3205d02ffa2c5e9f22c8f29d254
dhyeysanghavi <55806338+dhyeysanghavi@users.noreply.github.com>
2021-09-14 16:03:26 +0530

    Update App.js
    Implemented a basic API request from a random online database using axios so as to I do not have to explicitly transform the fetched data into JSON format.
    Furthermore, I have implemented two functions(getData() and delCard()) that commit changes to the react state (data). setData has to be used because it's a functional component.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios"
+import { useState } from 'react';
 
 function App() {
+  const[data,setData]=useState([]);
+  const getData=()=>{
+    return axios.get('https://jsonplaceholder.typicode.com/albums')
+    .then(function (response) {
+      // handle success
+      setData(response.data);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
+  }
+  const delCard=()=>{
+    setData(data.slice(1));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={getData}>hi</button>
+      <pre>{JSON.stringify(data)}</pre>
+      <button onClick={delCard}>bye</button>
     </div>
   );
 }
 
 export default App;
+
